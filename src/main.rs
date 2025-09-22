07daf375340eadc04b997998f087f8d3b779ab39
Sverre Johann Bjørke <sverrejb@gmail.com>
2025-09-22 22:48:55 +0200

    change url

--- src/main.rs
+++ src/main.rs
@@ -28,10 +28,8 @@ async fn main() {
     let app = Router::new()
         .route("/log", post(log_handler))
         .with_state(state);
-    println!("Listening on 127.0.0.1:3000");
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
-        .await
-        .unwrap();
+    println!("Listening on 0.0.0.0:3000");
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app.into_make_service())
         .await
         .unwrap();
